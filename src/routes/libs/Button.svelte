2eee39ba865ebf6067fe9338a967c41e0fd1df65
codernike <108070610+codernike@users.noreply.github.com>
2022-07-24 13:47:09 +0530

    calc svelte

---
+++ src/routes/libs/Button.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let onClick = () => {};
+  export let spanRow = false;
+  export let type = "number";
+</script>
+
+<style>
+  .spanRow {
+    grid-column: 1 / -1;
+  }
+
+  .number {
+    background-color: rgb(8, 72, 246);
+  }
+
+  .operator {
+    background-color: #ff4561;;
+  }
+
+  .action {
+    background-color: gray;
+  }
+
+  h2 {
+    color: #fff;
+  }
+</style>
+
+<button class={type} class:spanRow on:click={onClick}>
+  <h2>
+    <slot>¯\_(ツ)_/¯</slot>
+  </h2>
+</button>
